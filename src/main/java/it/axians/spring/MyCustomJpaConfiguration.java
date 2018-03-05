794a121f4cdce0d4596a2e1239626649f3697406
Giovanni Lovato <giovanni.lovato@axians.it>
2018-03-05 15:51:13 +0100

    Remove EnableTransactionManagement annotation

--- src/main/java/it/axians/spring/MyCustomJpaConfiguration.java
+++ src/main/java/it/axians/spring/MyCustomJpaConfiguration.java
@@ -2,10 +2,8 @@ package it.axians.spring;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @EnableJpaRepositories(repositoryFactoryBeanClass = MyCustomJpaRepositoryFactoryBean.class)
 public class MyCustomJpaConfiguration {
 }
